9bf683062fa448e9208e0e40a491ba7724566a47
FaadhilS <97407857+FaadhilS@users.noreply.github.com>
2025-11-07 15:58:04 +0530

    js/email-config.js

---
+++ js/email-config.js
@@ -0,0 +1,7 @@
+                                                                                             .       .                                       // Email configuration
+const EMAIL_CONFIG = {
+    SERVICE_ID: 'service_29dor6j', // Replace with your Service ID
+    TEMPLATE_ID: 'template_18cympj', // Replace with your Template ID
+    PUBLIC_KEY: 'jCOTajpd3keVZapOr', // Replace with your Public Key
+    DEFAULT_EMAIL: 'faadhilshibly@gmail.com' // Pre-agreed email
+};
